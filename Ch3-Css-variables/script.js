a8851f6626f9c2d098ec1489bf278e795038bce9
Ankita Srivastava <ankitacs0018@gmail.com>
2023-09-09 13:56:04 +0200

    final code

---
+++ Ch3-Css-variables/script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.controls input');
+function handleUpdate() {
+    const sizing = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + sizing)
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('change', handleUpdate)
+    input.addEventListener('mousemove', handleUpdate)
+})
